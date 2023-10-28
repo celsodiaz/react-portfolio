eaf376c9c380cfc8b2c212423d77e91b9739a0f2
celso <celso.acd.11@gmail.com>
2023-10-28 18:20:13 -0500

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import "./App.css";
+// import prueba from "../public/prueba.jpeg";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <div className="hero-header">
+      <div className="wrapper">
+        <header>
+          <div className="logo">
+            <i className="fa-solid fa-c"></i>
+            <div className="logo-text">Celso Diaz</div>
+          </div>
+          <nav>
+            <div className="togglebtn">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <ul className="navlinks">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Resume</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,34 @@
+import styled from "@emotion/styled";
+
+const Container = styled.header`
+  width: 100%;
+  height: 3em;
+  position: fixed;
+  top: 0;
+  background-color: cyan;
+  align-items: center;
+`;
+
+const SectionContainer = styled.section`
+  display: flex;
+  /* justify-content: space-between; */
+  margin: 0 auto;
+  /* align-items: center; */
+`;
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <SectionContainer>
+        <div>LOGO</div>
+        <div>
+          <a href="#about">Experiencia</a>
+          <a href="#proyectos">Proyectos</a>
+          <a href="#contacted">Contact me</a>
+        </div>
+      </SectionContainer>
+    </Container>
+  );
 };
 
 export default Header;
